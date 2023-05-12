972bf800f643d8f26bd6965637c2b0295e6d7188
alek <wait82985@gmail.com>
2023-05-12 14:26:49 +0600

    24/54 testcases passed

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,7 +1,27 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Solution {
     public long mostPoints(int[][] questions) {
+        long[] dp = new long[questions.length + 2];
+        dp[dp.length - 1] = 0;
+        for (int i = 2; i < dp.length ; i++) {
+            dp[dp.length - i] = Math.max(dp[dp.length - i + 1], findPreviousAvailableQuestion(questions, questions.length - i + 1) + questions[questions.length - i + 1][0]);
+        }
+        System.out.println(Arrays.toString(dp));
+        Arrays.sort(dp);
+        return dp[dp.length - 1];
+    }
 
+    private long findPreviousAvailableQuestion(int[][] questions, int currentIndex) {
+        int potentialIndex = currentIndex;
+        while(potentialIndex != -1){
+            if(potentialIndex + questions[potentialIndex][1] + 1 == currentIndex){
+                return questions[potentialIndex][0];
+            }
+            potentialIndex--;
+        }
+        return  0;
     }
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -4,8 +4,25 @@ import org.junit.Test;
 
 public class SolutionTest {
     @Test
-    public void mostPointsTest1(){
+    public void mostPointsTest1() {
         Assert.assertEquals(5, new Solution().mostPoints(
-                new int[][]{{3,2}, {4,3}, {4,4},{2,5}}));
+                new int[][]{{3, 2}, {4, 3}, {4, 4}, {2, 5}}));
+    }
+
+    @Test
+    public void mostPointsTest2() {
+        Assert.assertEquals(7, new Solution().mostPoints(
+                new int[][]{{1, 1}, {2, 2}, {3, 3}, {4, 4}, {5, 5}}));
+    }
+
+    @Test
+    public void mostPointsTest3() {
+        Assert.assertEquals(43, new Solution().mostPoints(
+                new int[][]{{43, 5}}));
+    }
+    @Test
+    public void mostPointsTest4() {
+        Assert.assertEquals(157, new Solution().mostPoints(
+                new int[][]{{21, 5}, {92, 3}, {74, 2}, {39, 4}, {58, 2}, {5, 5}, {49, 4}, {65, 3}}));
     }
 }
